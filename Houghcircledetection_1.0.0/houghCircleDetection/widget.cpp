ea0acb20294370cf8ea9111f52caf1ff4a88e2e9
huangJiaWei-new <huangjiawei2022222@163.com>
2023-08-17 10:15:47 +0800

    Houghcircledetection_1.0.0
    2023/8/17，霍夫圆检测初步完成(根据实际要求待优化)，未对.bmp(11.7M)完成检测。

---
+++ Houghcircledetection_1.0.0/houghCircleDetection/widget.cpp
@@ -0,0 +1,67 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include "opencv2/opencv.hpp"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+/*--------------@brief：点击'加载图像'按钮，显示图像-------------*/
+void Widget::on_displayImg_clicked()
+{
+    cv::Mat inputImage = cv::imread("D:/KeXinSuo/Houghcircledetection/Img1.bmp",
+                                    cv::IMREAD_COLOR);
+
+    cv::namedWindow("Image Viewer", cv::WINDOW_NORMAL); //创建一个可调整大小的窗口
+    cv::imshow("Image Viewer", inputImage); //'imshow'函数会调整图片大小以适应窗口大小？
+}
+
+
+/*--------------@brief：进行霍夫圆检测-------------*/
+void Widget::on_detectCircle_clicked()
+{
+    cv::Mat inputImg = cv::imread("D:/KeXinSuo/Houghcircledetection/Img3.png");
+
+    if(inputImg.empty())
+    {
+        qDebug()<<"Error: Could not read the image.";
+    }
+
+    cv::Mat grayImage, outputImg;
+    cv::cvtColor(inputImg, grayImage, cv::COLOR_BGR2GRAY); //转换为灰度图像
+
+    //霍夫圆检测
+    std::vector<cv::Vec3f> circles;
+    cv::HoughCircles(grayImage, circles, cv::HOUGH_GRADIENT, 1, grayImage.rows / 8, 200, 100, 0, 0);
+
+    inputImg.copyTo(outputImg); //原始输入图像复制到'outputImg',在其上绘制圆
+    // 在原始图像上绘制检测到的圆
+    for(size_t i = 0; i < circles.size(); i++)
+    {
+        cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+        int radius = cvRound(circles[i][2]);
+        cv::circle(outputImg, center, 3, cv::Scalar(0, 255, 0), -1, 8, 0); // 绘制圆心
+        cv::circle(outputImg, center, radius, cv::Scalar(0, 0, 255), 3, 8, 0); // 绘制圆
+    }
+
+    cv::namedWindow("outputImg", cv::WINDOW_NORMAL); //创建一个可调整大小的窗口
+    cv::imshow("outputImg", outputImg); //'imshow'函数会调整图片大小以适应窗口大小？
+}
+
+
+
+
+
+
+
+
+
